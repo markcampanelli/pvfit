12b6c9bf2c6fcc7157bf81dd6eb1bb79f9f50501
Mark Campanelli <mark.campanelli@gmail.com>
2019-08-09 03:08:20 -0600

    Rename package-level test file

--- pvfit_m/pvfit_m_test.py
+++ pvfit_m/pvfit_m_test.py
@@ -1,9 +1,11 @@
+"""Package tests."""
+
 import pvfit_m
 
 
-def test_imports():
+def test_package_imports():
     assert pvfit_m.core is not None
 
 
-def test_version():
+def test_package_version():
     assert pvfit_m.__version__ is not None
